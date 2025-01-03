341f6bbc8ef7b610aac57751443bbcd3846b558e
ArdorHoon <didgnsah7320@gmail.com>
2025-01-03 16:25:11 +0900

    n1027

---
+++ src/gold4/N1027.java
@@ -0,0 +1,16 @@
+package gold4;
+
+// url : https://www.acmicpc.net/problem/1027
+/*
+   Algorithm
+   * 수학
+   * 브루트포스 알고리즘
+   * 기하학
+ */
+public class N1027 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+}

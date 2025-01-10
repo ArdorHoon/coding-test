522181d447726158e14456e42276e5fbe7ad2909
ArdorHoon <didgnsah7320@gmail.com>
2025-01-10 15:20:38 +0900

    pccp question1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+
+
+/*
+어떤 수를 소수의 곱으로만 나타내는 것을 소인수분해라 하고, 이 소수들을 그 수의 소인수라고 합니다.
+예를 들면 13195의 소인수는 5, 7, 13, 29 입니다.
+
+600851475143의 소인수 중에서 가장 큰 수를 구하세요.
+ */
 public class Main {
     public static void main(String[] args) {
+            Double number = 600851475143D;
+
+            System.out.println( Math.sqrt(number));
+
+            for(Double i=2D ; i< Math.sqrt(number); i++){
+                if(number%i ==0){
+                    number = number/i;
+                }
+            }
+
+            //소인수 중 가장 큰 수
+            System.out.println(number);
 
     }
 }
--- src/programmers/pccp/PCCP_PastQuestion1.java
+++ src/programmers/pccp/PCCP_PastQuestion1.java
@@ -7,7 +7,106 @@ package programmers.pccp;
  */
 public class PCCP_PastQuestion1 {
     public static void main(String[] args) {
+        Solution solution = new Solution();
 
+        //Result : "06:55"
+        System.out.println(solution.solution("10:55", "00:05", "00:15", "06:55", new String[]{"prev", "next", "next"}));
+        //"07:22"	"04:05"	"00:15"	"04:07"	["next"]
+
+    }
+
+    public static class Solution {
+        public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+            String answer = alignCurrentPos(pos, video_len);
+
+            //오프닝 시간 확인
+            answer = alignOpeningPanel(answer, op_start, op_end);
+
+            for (String command : commands) {
+                switch (command) {
+                    case "next":
+                        String[] nAnswer = answer.split(":");
+                        int prevA = Integer.parseInt(nAnswer[0]);
+                        int nextA = Integer.parseInt(nAnswer[1]);
+                        nextA = nextA + 10;
+
+                        prevA = prevA + (nextA / 60);
+                        nextA = nextA % 60;
+
+
+                        if (prevA < 10) answer = "0" + prevA;
+                        else answer = Integer.toString(prevA);
+                        if (nextA >= 10) answer = answer + ":" + nextA;
+                        else answer = answer + ":0" + nextA;
+
+                        //현재 위치 확인
+                        answer = alignCurrentPos(answer, video_len);
+                        //오프닝 시간 확인
+                        answer = alignOpeningPanel(answer, op_start, op_end);
+                        break;
+                    case "prev":
+                        String[] pAnswer = answer.split(":");
+                        int prevB = Integer.parseInt(pAnswer[0]);
+                        int nextB = Integer.parseInt(pAnswer[1]);
+                        nextB = nextB - 10; // -13
+
+
+                        if (nextB < 0) {
+                            nextB = 60 + nextB;
+                            prevB = prevB - 1;
+                        }
+
+                        //만약 분이 -1 이 나오면 처음으로 돌아간다.
+                        if (prevB < 0) {
+                            prevB = 0;
+                            nextB = 0;
+                        }
+
+
+                        if (prevB < 10) {
+                            answer = "0" + prevB;
+                        } else {
+                            answer = Integer.toString(prevB);
+                        }
+
+                        if (nextB >= 10) {
+                            answer = answer + ":" + nextB;
+                        } else {
+                            answer = answer + ":0" + nextB;
+                        }
+
+                        //현재 위치 확인
+                        answer = alignCurrentPos(answer, video_len);
+                        //오프닝 시간 확인
+                        answer = alignOpeningPanel(answer, op_start, op_end);
+                        break;
+                }
+
+            }
+
+            return answer;
+        }
+
+        public String alignOpeningPanel(String pos, String op_start, String op_end) {
+            int cur = Integer.parseInt(pos.replace(":", ""));
+            int opStartNum = Integer.parseInt(op_start.replace(":", ""));
+            int opEndNum = Integer.parseInt(op_end.replace(":", ""));
+
+            if (cur >= opStartNum && cur <= opEndNum)
+                return op_end;
+
+            return pos;
+        }
+
+        public String alignCurrentPos(String pos, String video_len) {
+            String cPos = pos;
+            int cPosNum = Integer.parseInt(cPos.replace(":", ""));
+            int vlNum = Integer.parseInt(video_len.replace(":", ""));
+
+            if (cPosNum < 10) return "00:00";
+            else if (vlNum - cPosNum < 10) return video_len;
+            else return cPos;
+        }
     }
 
 }
